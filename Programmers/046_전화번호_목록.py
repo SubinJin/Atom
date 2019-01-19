120fb9c763886361bd2b162bc0546505261e41f4
SubinJin <jeensb0705@naver.com>
2019-01-19 17:48:31 +0900

    20190119

---
+++ Programmers/046_전화번호_목록.py
@@ -0,0 +1,16 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort(key=len)
+
+    for i in range(len(phone_book) - 1) :
+        for j in range(i +1, len(phone_book)) :
+            if phone_book[i] == phone_book[j][0 : len(phone_book[i])] :
+                answer = False
+                break
+        if answer == False :
+            break
+    #print(phone_book)
+    #print(phone_book[0])
+    #print(phone_book[1][0 : len(phone_book[0])])
+
+    return answer
